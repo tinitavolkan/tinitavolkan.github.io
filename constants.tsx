df99e36136f2658e767715a34859fcf1d8f5f1dd
tinitaVolkan <tinitavolkan@gmail.com>
2026-01-02 18:58:59 +0300

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { Category } from './types';
+
+export const CATEGORIES: Category[] = [
+  { id: 'all', name: 'Tüm Arşiv' },
+  { id: 'Hype', name: 'Hype' },
+  { id: 'Triel', name: 'Triel' },
+  { id: 'Sinasi', name: 'Şinasi' },
+  { id: 'Prensesperver', name: 'Prenses' },
+  { id: 'Nuriben', name: 'Nuriben' },
+  { id: 'Nesrin', name: 'Nesrin' },
+  { id: 'Egearseven', name: 'Ege' },
+  { id: 'Blush', name: 'Blush' }
+];
+
+export const FALLBACK_VIDEOS = [
+  { "title": "Hype - Turnuva Finali 01.01.2025", "embed_id": "v6323sc", "link": "#" },
+  { "title": "Triel - Epik Oyun Anları 01.01.2025", "embed_id": "v123abc", "link": "#" },
+  { "title": "Şinasi - Sohbet ve Muhabbet 15.12.2024", "embed_id": "v456def", "link": "#" },
+  { "title": "Prensesperver - Yeni Yıl Özel Yayını", "embed_id": "v789ghi", "link": "#" },
+  { "title": "Nuriben - Vlog #3 10.12.2024", "embed_id": "v321jkl", "link": "#" },
+  { "title": "Blush - Setup Turu 05.11.2024", "embed_id": "v654mno", "link": "#" },
+  { "title": "Egearseven - Teknoloji İncelemesi 22.10.2024", "embed_id": "v987pqr", "link": "#" },
+  { "title": "Nesrin - Canlı Yayın Kaydı 15.09.2024", "embed_id": "v147stu", "link": "#" },
+  { "title": "Triel - FPS İpuçları 01.09.2024", "embed_id": "v258vwx", "link": "#" }
+];
